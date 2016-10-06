20e804d9f0cb708b9b9e5f245d5385138143e2a2
upgraded-robot <mariadotdev.ln@gmail.com>
2016-10-06 11:43:54 -0500

    solucion simonDIce angie-maria

--- simonDice/public/js/application.js
+++ simonDice/public/js/application.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-  // Tu codigo va aca
+	$("#get_color").on("click", function(e){
+		var ul = $("ul").children()
+		var ranLi = ul[Math.floor(Math.random() * 9)]
+
+		var request = $.ajax({
+			method:"post",
+			url: "/color"
+		})
+
+		request.done(function(response){
+			$(ranLi).css({'background-color': response.color})
+			console.log($(ranLi))
+			debugger
+		})
+	})
+
+//console.log(ul)
 });
